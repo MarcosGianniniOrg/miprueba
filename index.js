855c81ed3c0044ca3adba2c50fa8f45df6780706
Marklenders1985 <marcogfa@gmail.com>
2024-11-07 13:09:36 +0100

    Añadida funcionalidad de arrastre a las tarjetas y ajustes en el footer

--- index.js
+++ index.js
@@ -15,3 +15,33 @@ nextButton.addEventListener("click", () => {
         behavior: "smooth"
     });
 });
+const projectsContainer = document.querySelector(".projects-container");
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+projectsContainer.addEventListener("mousedown", (e) => {
+    isDown = true;
+    projectsContainer.classList.add("active");
+    startX = e.pageX - projectsContainer.offsetLeft;
+    scrollLeft = projectsContainer.scrollLeft;
+});
+
+projectsContainer.addEventListener("mouseleave", () => {
+    isDown = false;
+    projectsContainer.classList.remove("active");
+});
+
+projectsContainer.addEventListener("mouseup", () => {
+    isDown = false;
+    projectsContainer.classList.remove("active");
+});
+
+projectsContainer.addEventListener("mousemove", (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - projectsContainer.offsetLeft;
+    const walk = (x - startX) * 2; // Velocidad de desplazamiento
+    projectsContainer.scrollLeft = scrollLeft - walk;
+});
