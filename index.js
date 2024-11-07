2582657ad7bf73b9eca58f6b860eca0ea2bc5145
Marklenders1985 <marcogfa@gmail.com>
2024-11-07 12:47:38 +0100

    Actualización de diseño y funcionalidad de scroll en el portafolio

--- index.js
+++ index.js
@@ -2,48 +2,16 @@ const prevButton = document.querySelector(".carousel-button.prev");
 const nextButton = document.querySelector(".carousel-button.next");
 const projectsContainer = document.querySelector(".projects-container");
 
-let isDown = false;
-let startX;
-let scrollLeft;
-
-projectsContainer.addEventListener("mousedown", (e) => {
-    isDown = true;
-    projectsContainer.classList.add("active");
-    startX = e.pageX - projectsContainer.offsetLeft;
-    scrollLeft = projectsContainer.scrollLeft;
-    projectsContainer.style.cursor = "grabbing"; // Cambia el cursor al agarrar
-});
-
-projectsContainer.addEventListener("mouseleave", () => {
-    isDown = false;
-    projectsContainer.classList.remove("active");
-    projectsContainer.style.cursor = "grab"; // Cambia el cursor de nuevo
-});
-
-projectsContainer.addEventListener("mouseup", () => {
-    isDown = false;
-    projectsContainer.classList.remove("active");
-    projectsContainer.style.cursor = "grab";
-});
-
-projectsContainer.addEventListener("mousemove", (e) => {
-    if (!isDown) return;
-    e.preventDefault();
-    const x = e.pageX - projectsContainer.offsetLeft;
-    const walk = (x - startX) * 2; // Ajusta la sensibilidad
-    projectsContainer.scrollLeft = scrollLeft - walk;
-});
-
 prevButton.addEventListener("click", () => {
     projectsContainer.scrollBy({
-        left: -projectsContainer.clientWidth,
-        behavior: "smooth",
+        left: -300,
+        behavior: "smooth"
     });
 });
 
 nextButton.addEventListener("click", () => {
     projectsContainer.scrollBy({
-        left: projectsContainer.clientWidth,
-        behavior: "smooth",
+        left: 300,
+        behavior: "smooth"
     });
 });
