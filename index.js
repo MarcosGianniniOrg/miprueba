7200fc60778b753ac3fef04064ad26ab991a2c15
Marklenders1985 <marcogfa@gmail.com>
2024-11-07 11:51:14 +0100

    Actualización completa de archivos con mejoras de navegación y estilo

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
-const prevButton = document.querySelector(".carousel-button.prev");
-const nextButton = document.querySelector(".carousel-button.next");
-const projectsContainer = document.querySelector(".projects-container");
-
-let scrollAmount = 0;
-const cardWidth = 250 + 20; // Ancho de la tarjeta + margen
-
-prevButton.addEventListener("click", () => {
-    scrollAmount -= cardWidth;
-    if (scrollAmount < 0) {
-        scrollAmount = 0;
+function scrollProjects(direction) {
+    const container = document.querySelector('.projects-container');
+    const scrollAmount = 300;
+    if (direction === 'left') {
+        container.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+    } else if (direction === 'right') {
+        container.scrollBy({ left: scrollAmount, behavior: 'smooth' });
     }
-    projectsContainer.scrollTo({
-        left: scrollAmount,
-        behavior: "smooth"
-    });
-});
-
-nextButton.addEventListener("click", () => {
-    scrollAmount += cardWidth;
-    if (scrollAmount > projectsContainer.scrollWidth - projectsContainer.clientWidth) {
-        scrollAmount = projectsContainer.scrollWidth - projectsContainer.clientWidth;
-    }
-    projectsContainer.scrollTo({
-        left: scrollAmount,
-        behavior: "smooth"
-    });
-});
+}
