1b61d6b336768c00078c0b90b94b5d251f6a970a
Marklenders1985 <marcogfa@gmail.com>
2025-01-14 18:29:46 +0100

    Actualizaciones en miprueba

---
+++ Apuntes y ejercicios del master/Unidad 2 - Lenguaje/1 Fundamentos/1.1 Tipos de datos/playground.js
@@ -0,0 +1,20 @@
+// 7 tipos PRIMITIVOS + Objetos + Funciones
+
+// String, number, boolean, null, undefined
+// symbol, bigInt
+
+// Objets, arrays
+
+// Function
+
+function myFunction(arg1, arg2) {
+    console.log(arg1, arg2);
+    return arg1;
+}
+
+myFunction("Hola");
+
+// Typeof
+
+const myData = true;
+console.log(typeof myData);
